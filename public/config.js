fecf9e726887d6fa0cff34bba936d9aeab44cea2
changfeng <changfeng@admin123.com>
2025-05-21 10:44:43 +0800

    feat: config.js配置文件和index.html同级目录，没有原样复制整个public文件夹到dist包中（版本v1）

---
+++ public/config.js
@@ -0,0 +1,6 @@
+var GLOBAL = {
+  /* 苏州安岭--项目配置 */
+  UserName: 'appid-24001-001-我是自定义配置化数据',
+  Password: '74F33CC7789F07AE7CB96896A0A3E539',
+  Host: 'https://api.helotus.cn'
+}
--- src/stores/src/user.ts
+++ src/stores/src/user.ts
@@ -11,6 +11,8 @@ export const useUserStore = defineStore("user", () => {
   })
 
   const userLogin = async (params) => {
+    // @ts-ignore
+    console.log(GLOBAL, 'GLOBAL---这里就可以读取全局配置化文件信息了, 但是这是最简单的方式。config.js配置文件和index.html同级目录')
     const res = await login(params)
     const {accesstoken, nickName, userId} = res?.data?.data
     userInfo.value = {
